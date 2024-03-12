35e495857681bda1415217e9051e5acfe7bb43c8
Miroslav Shilov <shilovmiroslav@abv.bg>
2024-03-12 18:19:43 +0200

    Override ProductStorage class to be possible to get Product by given rules(Unit).Add to Product class more constants in enum Unit.

--- src/restaurant/building_blocks/room/kitchen/ProductStorage.java
+++ src/restaurant/building_blocks/room/kitchen/ProductStorage.java
@@ -4,6 +4,7 @@ package restaurant.building_blocks.room.kitchen;
 import restaurant.building_blocks.Product;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ProductStorage {
 
@@ -13,7 +14,7 @@ public class ProductStorage {
         container = new HashMap<>();
     }
 
-    public int getStock(String productName) {
+    public double getStock(String productName) {
         if (container.containsKey(productName)) {
             Shaft shaft = container.get(productName);
             return shaft.getQuantity();
@@ -21,25 +22,38 @@ public class ProductStorage {
         return 0;
     }
 
-    public void addProduct(Product product, int quantity) {
+    public void addProduct(Product product, Product.Unit unit, double quantityPerUnit, int productsCount) {
+        double quantity = quantityPerUnit * unit.getValue();
         if (container.containsKey(product.getName())) {
             Shaft shaft = container.get(product.getName());
-            shaft.add(quantity);
+            shaft.add(quantity, productsCount);
         } else {
-            Shaft newShaft = new Shaft();
-            newShaft.add(quantity);
+            Shaft newShaft = new Shaft(unit);
+            newShaft.add(quantity, productsCount);
             container.put(product.getName(), newShaft);
         }
     }
 
-    public void getProduct(Product product, int quantity) throws ProductOutOfStockException {
+    public void getProduct(Product product, Product.Unit unit, double quantity) throws ProductOutOfStockException {
         if (quantity > 0) {
             if (container.containsKey(product.getName())) {
                 Shaft shaft = container.get(product.getName());
-                if (quantity <= shaft.getQuantity()) {
-                    shaft.get(quantity);
+                double totalQuantity = quantity * unit.getValue();
+                double shaftQuantity = shaft.getQuantity();
+
+                if (unit.equals(Product.Unit.Брой)) {
+
+                    if (shaft.getProductsCount() >= totalQuantity) {
+                        shaft.getPiece(totalQuantity);
+                    } else {
+                        throw new ProductOutOfStockException("Продукта е изчерпан");
+                    }
                 } else {
-                    throw new ProductOutOfStockException("Продукта е изчерпан");
+                    if (shaftQuantity >= totalQuantity) {
+                        shaft.get(totalQuantity);
+                    } else {
+                        throw new ProductOutOfStockException("Продукта е изчерпан");
+                    }
                 }
             } else {
                 throw new ProductOutOfStockException("Продукта е изчерпан");
@@ -47,21 +61,58 @@ public class ProductStorage {
         }
     }
 
-    public static class Shaft {
-        private int quantity;
+    public void printStock() {
+        for (Map.Entry<String, Shaft> record : container.entrySet()) {
+            Shaft s = record.getValue();
+            System.out.println(record.getKey() + " | " + s.quantity + " " +
+                    s.quantityUnit + "  " + s.getProductsCount());
+        }
+    }
 
-        public int getQuantity() {
+    public static class Shaft {
+        private double quantity;
+
+        public Shaft(Product.Unit quantityUnit) {
+            this.quantityUnit = quantityUnit;
+        }
+
+        public Product.Unit getQuantityUnit() {
+            return quantityUnit;
+        }
+
+
+        private Product.Unit quantityUnit;
+
+        public double getProductsCount() {
+            return productsCount;
+        }
+
+        private double productsCount;
+
+        public double getQuantity() {
             return quantity;
         }
 
-        public void add(int quantity) {
+        public void add(double quantity, double productsCount) {
+            this.productsCount += productsCount;
             this.quantity += quantity;
         }
 
-        public void get(int quantity) {
-            if (this.quantity >= quantity) {
-                this.quantity -= quantity;
-            }
+        public void get(double quantity) {
+            double quantityForSingleProduct = (this.quantity / productsCount);
+            this.quantity -= quantity;
+
+            double productCountForGivenQuantity = quantity / quantityForSingleProduct;
+
+            productsCount -= productCountForGivenQuantity;
+        }
+
+        public void getPiece(double count) {
+            double quantityForSingleProduct = quantity / productsCount;
+            this.quantity -= quantityForSingleProduct * count;
+            String formatted = String.format("%.1f", quantity);
+            quantity = Double.parseDouble(formatted.replaceAll(",", "."));
+            productsCount -= count;
         }
     }
 }
--- src/restaurant/test/test_rooms/test_kitchen/Test_ProductStorage.java
+++ src/restaurant/test/test_rooms/test_kitchen/Test_ProductStorage.java
@@ -18,46 +18,92 @@ public class Test_ProductStorage {
 
     @Test
     public void test_getStock() throws ProductOutOfStockException {
-        Assert.assertEquals(0, storage.getStock("Картофи"));
-        Product potatoes = new Product("Картофи", Product.Unit.Грам, 23);
-        storage.addProduct(potatoes, 1000);
-        Assert.assertEquals(1000, storage.getStock("Картофи"));
-        storage.getProduct(potatoes, 1000);
-        Assert.assertEquals(0, storage.getStock("Картофи"));
+        Assert.assertEquals(0, storage.getStock("Картофи"), 0);
+
+        Product лук = new Product("лук", Product.Unit.Грам, 3.60);
+        storage.addProduct(лук, Product.Unit.Грам, 1000, 10);
+        Assert.assertEquals(1000, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Брой, 1);
+        Assert.assertEquals(900, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Грам, 100);
+        Assert.assertEquals(800, storage.getStock("лук"), 0);
+
+        storage.printStock();
+        storage.getProduct(лук, Product.Unit.Брой, 1);
+        Assert.assertEquals(700, storage.getStock("лук"), 0);
+
     }
 
     @Test
     public void test_addProducts() {
-        Product potatoes = new Product("Картофи", Product.Unit.Грам, 43);
-        storage.addProduct(potatoes, 1000);
-        storage.addProduct(potatoes, 500);
-        int productStock = storage.getStock("Картофи");
-        Assert.assertEquals(1500, productStock);
+        Product лук = new Product("лук", Product.Unit.Грам, 4);
+        storage.addProduct(лук, Product.Unit.Грам, 5000, 10);
+
+
+        Assert.assertEquals(5000, storage.getStock("лук"), 1);
+        double productStock = storage.getStock("лук");
+        Assert.assertEquals(5000, productStock, 1);
     }
 
     @Test
     public void test_getProducts() throws ProductOutOfStockException {
-        Product potatoes = new Product("Картофи", Product.Unit.Грам, 2);
-        storage.addProduct(potatoes, 1000);
-        storage.addProduct(potatoes, 1000);
-        storage.getProduct(potatoes, 999);
-        storage.getProduct(potatoes, 1000);
-        int productStock = storage.getStock("Картофи");
-        Assert.assertEquals(1, productStock);
+        Product лук = new Product("лук", Product.Unit.Грам, 4);
+
+        storage.addProduct(лук, Product.Unit.Грам, 1000, 10);
+        storage.getProduct(лук, Product.Unit.Брой, 1);
+        storage.printStock();
+        Assert.assertEquals(900, storage.getStock("лук"), 0);
+
+
+        storage.getProduct(лук, Product.Unit.Грам, 100);
+        Assert.assertEquals(800, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Брой, 1);
+        Assert.assertEquals(700, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Грам, 250);
+        storage.printStock();
+        Assert.assertEquals(450, storage.getStock("лук"), 0);
+
+        storage.getProduct(лук, Product.Unit.Брой, 1);
+        Assert.assertEquals(350, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Килограм, 0.100);
+        Assert.assertEquals(250, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Кафена_чаша, 1);
+        Assert.assertEquals(150, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Чаена_лъжица, 1);
+        Assert.assertEquals(130, storage.getStock("лук"), 0);
+        storage.printStock();
+
+        storage.getProduct(лук, Product.Unit.Супена_лъжица, 1);
+        Assert.assertEquals(100, storage.getStock("лук"), 0);
+        storage.printStock();
     }
 
     @Test
     public void test_ProductOutOfStockException() throws ProductOutOfStockException {
         Product potatoes = new Product("Картофи", Product.Unit.Грам, 43);
         Assert.assertThrows(ProductOutOfStockException.class, () -> {
-            storage.getProduct(potatoes, 1);
+            storage.getProduct(potatoes, Product.Unit.Брой, 1);
         });
 
-        storage.addProduct(potatoes, 1000);
-        storage.getProduct(potatoes, 1000);
+        storage.addProduct(potatoes, Product.Unit.Грам, 1, 5);
+        storage.getProduct(potatoes, Product.Unit.Брой, 1000);
 
         Assert.assertThrows(ProductOutOfStockException.class, () -> {
-            storage.getProduct(potatoes, 1);
+            storage.getProduct(potatoes, Product.Unit.Брой, 1);
         });
     }
 }
